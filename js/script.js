c9e6ae5444c0919fff1dfcee3fb3f58372127bd9
nmelgar <nmelgar16013@gmail.com>
2021-11-09 17:19:31 -0600

    09-11-17:18--changes to styles, the function to paint works with 16x16 grid

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
 const container = document.getElementById("main-content");
 
 function eightSquares() {
-    for (i = 0; i < 256; i++) {
-        const squareDiv = document.createElement("div");
-        squareDiv.classList.add('squareDiv');
-        container.appendChild(squareDiv);
-    }
+  for (i = 0; i < 64; i++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.classList.add('squareDiv');
+    // squareDiv.id = "individual-square"
+    container.appendChild(squareDiv);
+  }
 }
 
-eightSquares();
+function sixteenSquares() {
+  for (i = 0; i < 256; i++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.classList.add('squareDiv');
+    // squareDiv.id = "individual-square"
+    container.appendChild(squareDiv);
+  }
+}
+sixteenSquares();
+
+
+function paintBlue() {
+  let individualSquare = document.querySelectorAll(".squareDiv");
+  for (let j = 0; j < individualSquare.length; j++) {
+    individualSquare[j].addEventListener("mouseover", function paint() {
+      individualSquare[j].style.backgroundColor = "blue";
+    });
+  }
+}
+
+paintBlue();
