bc6079d489af316bfb140f83aa340a2258def2c3
nmelgar <nmelgar16013@gmail.com>
2021-11-08 20:00:13 -0600

    08-11-19-59--Styles added, more changes needed

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@ const container = document.getElementById("main-content");
 
 function eightSquares() {
     for (i = 0; i < 256; i++) {
-
         const squareDiv = document.createElement("div");
         squareDiv.classList.add('squareDiv');
         container.appendChild(squareDiv);
-
     }
 }
 
