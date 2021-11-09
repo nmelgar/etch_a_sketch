5fb564fee07aeed432f976aca8f3d1b4b7189d19
NM <38670052+nmelgar@users.noreply.github.com>
2021-11-08 18:38:00 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const container = document.getElementById("main-content");
+
+function eightSquares() {
+    for (i = 0; i < 256; i++) {
+
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.add('squareDiv');
+        container.appendChild(squareDiv);
+
+    }
+}
+
+eightSquares();
