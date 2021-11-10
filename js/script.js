af61b245b63343c1f990101e875caa55f8737386
nmelgar <nmelgar16013@gmail.com>
2021-11-10 11:41:30 -0600

    10-11-11:41--buttons added, paint function working, trying to figure out how to make with other grid sizes

--- js/script.js
+++ js/script.js
@@ -16,17 +16,36 @@ function sixteenSquares() {
     // squareDiv.id = "individual-square"
     container.appendChild(squareDiv);
   }
+  paintBlack();
 }
+
 sixteenSquares();
 
+let gridSizeButton = document.getElementById("grid-size-button").addEventListener("click", chooseGrid);
 
-function paintBlue() {
+//Allow the user to choose the desired grid to draw
+function chooseGrid() {
+  let userChose = prompt("Choose a number between 8, 16, 32 and 64");
+  if (userChose == 8) {
+    eightSquares();
+  } else if (userChose == 16) {
+    sixteenSquares();
+  } else if (userChose == 32) {
+    console.log("You chose the right grid");
+  } else if (userChose == 64) {
+    console.log("You chose the right grid");
+  } else if (userChose != 8, 16, 32, 64)
+    alert("Please, choose a valid grid!")
+
+}
+
+//function to paint black squares
+function paintBlack() {
   let individualSquare = document.querySelectorAll(".squareDiv");
   for (let j = 0; j < individualSquare.length; j++) {
     individualSquare[j].addEventListener("mouseover", function paint() {
-      individualSquare[j].style.backgroundColor = "blue";
+      individualSquare[j].style.backgroundColor = "#000000";
     });
   }
 }
 
-paintBlue();
