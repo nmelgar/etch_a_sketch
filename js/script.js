23a299071c6f35c96843862711389fa014a61725
nmelgar <nmelgar16013@gmail.com>
2021-11-11 17:37:02 -0600

    11-11-17:36--starting again to make the content dynamic instead of adding element by element style

--- js/script.js
+++ js/script.js
@@ -1,16 +1,13 @@
 const container = document.getElementById("main-content");
 
-function eightSquares() {
-  for (i = 0; i < 64; i++) {
-    const squareDiv = document.createElement("div");
-    squareDiv.classList.add('squareDiv');
-    // squareDiv.id = "individual-square"
-    container.appendChild(squareDiv);
-  }
-}
+//number of rows == number of columns
+//user introduces number of rows
+
+let userChose = prompt("Choose a number between 8, 16, 32 and 64");
+const gridCounter = userChose * userChose;
 
 function sixteenSquares() {
-  for (i = 0; i < 256; i++) {
+  for (i = 0; i < gridCounter; i++) {
     const squareDiv = document.createElement("div");
     squareDiv.classList.add('squareDiv');
     // squareDiv.id = "individual-square"
@@ -23,21 +20,6 @@ sixteenSquares();
 
 let gridSizeButton = document.getElementById("grid-size-button").addEventListener("click", chooseGrid);
 
-//Allow the user to choose the desired grid to draw
-function chooseGrid() {
-  let userChose = prompt("Choose a number between 8, 16, 32 and 64");
-  if (userChose == 8) {
-    eightSquares();
-  } else if (userChose == 16) {
-    sixteenSquares();
-  } else if (userChose == 32) {
-    console.log("You chose the right grid");
-  } else if (userChose == 64) {
-    console.log("You chose the right grid");
-  } else if (userChose != 8, 16, 32, 64)
-    alert("Please, choose a valid grid!")
-
-}
 
 //function to paint black squares
 function paintBlack() {
